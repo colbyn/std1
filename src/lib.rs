6aa9f5f344d3366c21d0b618ceb78b5afa1f2471
Colbyn Wadman <hello@colbyn.com>
2023-02-12 17:45:33 -0700

    add ToRef trait

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,12 @@ extern crate either as either_lib;
 pub mod either {
     pub use ::either_lib::*;
 }
+
+pub trait ToRef {
+    /// The dedicated type with references therein.
+    type Type<'a>: Clone where Self: 'a;
+    fn to_ref<'a>(&'a self) -> Self::Type<'a>;
+
+    /// Semantic alias for `ToRef::to_ref`.
+    fn to_own_lendable<'a>(&'a self) -> Self::Type<'a> {self.to_ref()}
+}
